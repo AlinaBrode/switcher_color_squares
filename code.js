842b3739472cd08b445ce5afebdb79e1103996d6
Alina <alinbrode@yandex.ru>
2025-06-26 11:19:57 +0300

    add new files

---
+++ code.js
@@ -0,0 +1,24 @@
+function setDiv1Aqua(){
+  document.querySelector("#id_div_1").style.backgroundColor="aquamarine";
+}
+function setDiv1Chart(){
+  document.querySelector("#id_div_1").style.backgroundColor="chartreuse"
+}
+function setDiv2Aqua(){
+  document.querySelector("#id_div_2").style.backgroundColor="aquamarine";
+}
+function setDiv2Chart(){
+  document.querySelector("#id_div_2").style.backgroundColor="chartreuse"
+}
+function setDiv3Aqua(){
+  document.querySelector("#id_div_3").style.backgroundColor="aquamarine";
+}
+function setDiv3Chart(){
+  document.querySelector("#id_div_3").style.backgroundColor="chartreuse"
+}
+function setDiv4Aqua(){
+  document.querySelector("#id_div_4").style.backgroundColor="aquamarine";
+}
+function setDiv4Chart(){
+  document.querySelector("#id_div_4").style.backgroundColor="chartreuse"
+}
